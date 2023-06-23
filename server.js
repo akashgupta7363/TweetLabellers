b70cfc3c329926c591a76191baa0af82e9e4d73c
Akash Gupta <akashgupta7363@gmail.com>
2023-06-23 19:31:37 +0530

    DB connected

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+
 dotenv.config();
+const connectDB = require('./config/db');
+connectDB();
 app.get('/', (req, res) => {
   res.send(`API is runningon ${process.env.PORT}`);
 });
