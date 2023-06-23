7bad0bb8290dfc4a58207b9c44cb20bd0d7e2e59
Akash Gupta <akashgupta7363@gmail.com>
2023-06-23 20:34:31 +0530

    tweetlist

---
+++ tweetList.js
@@ -0,0 +1,13 @@
+const tweetList = [
+  {
+    body: 'Excited to be a speaker for the public meeting on #AI on May 19. Info below: #PCAST Working Group on Generative AI Invites Public Input ',
+    username: `drfeifei`,
+  },
+  {
+    body: 'Facebookresearch released audiocraft!A PyTorch library for deep learning research on audio generation. It contains the code for MusicGen, a state-of-the-art controllable music generation LM for text-to-music.',
+    username: `AssemblyAI`,
+  },
+  {},
+];
+
+module.exports = tweetList;
